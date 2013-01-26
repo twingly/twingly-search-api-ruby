a74fa9dc05eade8ea492a0824cf9930dc61842b0
Johan Eckerström <johan@duh.se>
2013-01-26 12:57:30 +0100

    Let Parser create Result

--- lib/twingly-analytics/parser.rb
+++ lib/twingly-analytics/parser.rb
@@ -1,6 +1,13 @@
+require 'nokogiri'
+
 module Twingly
   module Analytics
     class Parser
+      def parse(document)
+        result = Result.new
+        result.posts = Nokogiri::XML(document).xpath('//post').to_a
+        result
+      end
     end
   end
 end
--- lib/twingly-analytics/result.rb
+++ lib/twingly-analytics/result.rb
@@ -1,15 +1,7 @@
-require 'nokogiri'
-
 module Twingly
   module Analytics
     class Result
-      def initialize(response_body = nil)
-        @document = Nokogiri::XML(response_body)
-      end
-
-      def posts
-        @document.xpath('//post').to_a
-      end
+      attr_accessor :posts
     end
   end
 end
