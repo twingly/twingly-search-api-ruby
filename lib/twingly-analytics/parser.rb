b31f9a9f3ce9abf3b2e215578455e936442f4c7a
Johan Eckerström <johan@duh.se>
2013-01-28 08:51:54 +0100

    Add posts as Hash

--- lib/twingly-analytics/parser.rb
+++ lib/twingly-analytics/parser.rb
@@ -5,8 +5,18 @@ module Twingly
     class Parser
       def parse(document)
         result = Result.new
-        result.posts = Nokogiri::XML(document).xpath('//post').to_a
+        Nokogiri::XML(document).xpath('//post').each do |post|
+          result.posts << parse_post(post)
+        end
         result
+      end
+    private
+      def parse_post(element)
+        post = {}
+        element.element_children.each do |child|
+          post[child.name] = child.text
+        end
+        post
       end
     end
   end
--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -3,11 +3,22 @@ require 'spec_helper'
 include Twingly::Analytics
 
 describe Parser do
-  context "with valid result" do
-    let(:document) { File.read('spec/fixtures/valid_result.xml') }
-    it { should respond_to(:parse) }
-    it "should parse document" do
-      subject.parse(document).should be_a Result
+  it { should respond_to(:parse) }
+
+  describe "#parse" do
+    subject { Parser.new }
+
+    context "with valid result" do
+      let(:document) { File.read('spec/fixtures/valid_result.xml') }
+
+      it "should parse document" do
+        subject.parse(document).should be_a Result
+      end
+
+      # TODO testa någon annanstans
+      it "should return posts" do
+        subject.parse(document).posts.should be_a Array
+      end
     end
   end
 end
