d4c363f3e6043b9fed7cd605710a575913b64f66
Johan Eckerström <johan@duh.se>
2013-07-11 13:53:42 +0200

    Add a working example

---
+++ examples/find_all_blogs_that_mentioned_twingly.rb
@@ -0,0 +1,28 @@
+require 'set'
+require 'bundler'
+Bundler.require
+
+client = Twingly::Analytics::Client.new('<API_KEY>')
+
+finished   = nil
+start_time = Time.new(2013,01,01)
+blogs      = Set.new
+
+until finished
+  query = client.query
+  query.pattern = "sort-order:asc sort:published twingly"
+  query.start_time = start_time
+  query.document_language = 'sv'
+  result = query.execute
+
+  result.posts.each do |post|
+    blogs.add(post.blog_url)
+  end
+
+  finished = start_time == result.posts.last.published
+  start_time = result.posts.last.published
+end
+
+blogs.each do |url|
+  puts url
+end
