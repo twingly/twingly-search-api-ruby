d19a69974c4d24b06b2a444eeae817d068eca921
Patrik Ragnarsson <patrik@starkast.net>
2015-09-12 16:43:51 +0200

    Make hello world example more friendly

--- examples/hello_world.rb
+++ examples/hello_world.rb
@@ -4,7 +4,7 @@ Bundler.require
 client = Twingly::Analytics::Client.new
 query = client.query
 query.pattern = '"hello world"'
-query.start_time = Time.now - 86400
+query.start_time = Time.now - (24 * 3600) # search last day
 result = query.execute
 result.posts.each do |post|
   puts post.url
