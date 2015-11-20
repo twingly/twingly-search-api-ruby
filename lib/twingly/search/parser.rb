f12e31a6a98324008aeb2ef1fa9f37d87413945e
Robin Wallin <robin@twingly.com>
2015-11-20 17:21:27 +0100

    Instantiate result when needed

--- lib/twingly/search/parser.rb
+++ lib/twingly/search/parser.rb
@@ -4,12 +4,12 @@ module Twingly
   module Search
     class Parser
       def parse(document)
-        result = Result.new
         nokogiri = Nokogiri::XML(document)
 
         failure = nokogiri.at_xpath('//name:blogstream/name:operationResult[@resultType="failure"]', name: 'http://www.twingly.com')
         fail failure.text if failure
 
+        result = Result.new
         result.number_of_matches_returned = nokogiri.at_xpath('/twinglydata/@numberOfMatchesReturned').value.to_i
         result.number_of_matches_total = nokogiri.at_xpath('/twinglydata/@numberOfMatchesTotal').value.to_i
         result.seconds_elapsed = nokogiri.at_xpath('/twinglydata/@secondsElapsed').value.to_f
