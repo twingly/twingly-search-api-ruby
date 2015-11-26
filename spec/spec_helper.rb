1bf9b581a8f848c805dabdbb7892b99c5e7772af
Mattias Roback <mattias.roback@twingly.com>
2015-11-26 10:44:55 +0100

    Add tests for all types of server responses
    Since its the parser that determines whether an error was returned
    from the server there is no need for having VCR cassettes.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,8 @@
 require File.dirname(__FILE__) + '/../lib/twingly/search'
+
+class Fixture
+  def self.get(fixture_name)
+    filename = "spec/fixtures/#{fixture_name}_result.xml"
+    File.read(filename)
+  end
+end
